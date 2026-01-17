44c2277d54c20a8194f7fa29ab332ccfac2812a6
abin <abin.z@outlook.com>
2026-01-17 23:01:58 +0800

    feat: 添加clang-tidy

--- src/add.cpp
+++ src/add.cpp
@@ -1,12 +1,5 @@
 #include "add.h"
 
+int add(int a, int b) { return a + b; }
 
-int add(int a, int b)
-{
-  return a + b;
-}
-
-double add(double a, double b)
-{
-  return a + b;
-}
+double add(double a, double b) { return a + b; }
